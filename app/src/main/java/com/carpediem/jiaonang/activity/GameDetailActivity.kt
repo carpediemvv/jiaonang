569978cfa51c3db64f00d13098290f7a2b133b16
weiwei <240027695@qq.com>
2018-07-24 16:25:30 +0800

    init homeFragment gameDetailActivity

---
+++ app/src/main/java/com/carpediem/jiaonang/activity/GameDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.carpediem.jiaonang.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.carpediem.jiaonang.R
+
+class GameDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_detail)
+    }
+}
